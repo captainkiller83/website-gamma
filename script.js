b0d9a0bca23ac19e76f63b5e5d852d5ca0d14bc8
Marvin Fastenau <marvin@apfelkuchenwege.tk>
2023-06-18 23:02:05 +0200

    wo bin ich mit den parallax denn nu gelandet

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ console.log('Hello!');
 document.querySelectorAll('path').forEach((path) => {
    path.style.setProperty('--length', path.getTotalLength().toString());
 });
+
+document.onscroll = () => {
+    document.querySelectorAll('.parallax').forEach((parallax) => {
+        parallax.animate([
+            { transform: `translateY(${window.scrollY * parallax.dataset.factor}px)` }
+        ], {
+            duration: parseInt(parallax.dataset.duration),
+            fill: 'forwards'
+        });
+    });
+
+    document.querySelectorAll('.fade-in').forEach((fade) => {
+        if (window.scrollY > fade.offsetTop - window.innerHeight) {
+            fade.style.setProperty('--opacity', (window.scrollY - fade.offsetTop + window.innerHeight) / window.innerHeight);
+        }
+    });
+
+    document.querySelectorAll('.fade-out').forEach((fade) => {
+    if (window.scrollY > fade.offsetTop - window.innerHeight) {
+                fade.style.setProperty('--opacity', 1 - (window.scrollY - fade.offsetTop + window.innerHeight) / window.innerHeight);
+            }
+    });
+}
