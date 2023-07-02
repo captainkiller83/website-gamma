6996e41dc7c112f6e116a00d4510e6d830180005
Marvin Fastenau <marvin@apfelkuchenwege.tk>
2023-07-02 21:22:02 +0200

    Ahhhh jetzt muss Leo nicht mehr geschlagen werden

--- script.js
+++ script.js
@@ -72,6 +72,6 @@ for (let element of parallaxElements) {
         orientation: element.dataset.orientation ? element.dataset.orientation : 'down',
         scale: (element.dataset.factor+1)*1.5,
         overflow: true,
-        delay: 0.5,
+        delay: element.dataset.duration/100,
     });
 }
